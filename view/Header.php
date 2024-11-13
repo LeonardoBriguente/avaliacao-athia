ea17ab5c6f7774e8ad360f1664479b813791a72f
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-13 08:49:42 -0300

    Estilização do header, h1 e nome do sistema

---
+++ view/Header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/Header.css">
+    <title></title>
+</head>
+<body>
+    <header class="cabecalho">
+        <img class="logo" src="assets/images/logo.png">
+        <h1 class="nome-sistema">SmartGestão</h1>
+    </header>
+</body>
+</html>
