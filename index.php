f858f7638d3c215ca092fbe622c200543545f1b4
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-13 15:14:14 -0300

    Configuração de meta tags

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Gestão, empresa, setor">
+    <meta name="author" content="Leonardo Briguente">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>SmartGestão</title>
+</head>
+<body>
+    
+</body>
+</html>
--- view/Header.php
+++ view/Header.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Gestão, empresa, setor">
+    <meta name="author" content="Leonardo Briguente">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/header.css">
     <title></title>
