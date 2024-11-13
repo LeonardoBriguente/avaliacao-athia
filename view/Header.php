86554070db80b91b0d2a76dde41b82ede290a288
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-13 09:28:02 -0300

    adição e estilização dos links e criação de divs de divisão

--- view/Header.php
+++ view/Header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,20 @@
     <link rel="stylesheet" type="text/css" href="assets/css/Header.css">
     <title></title>
 </head>
+
 <body>
     <header class="cabecalho">
-        <img class="logo" src="assets/images/logo.png">
-        <h1 class="nome-sistema">SmartGestão</h1>
+        <div class="itens-logo">
+            <img class="logo" src="assets/images/logo.png">
+            <h1 class="nome-sistema"><a class="titulo" href="/index.php">SmartGestão</a></h1>
+        </div>
+
+        <div class="caminhos">
+            <a class="link" href="CadastrarEmpresa.php">Empresas</a>
+            <a class="link link-meio" href="CadastrarSetores.php">Setores</a>
+            <a class="link" href="Relatorios.php">Relatórios</a>
+        </div>
     </header>
 </body>
+
 </html>
