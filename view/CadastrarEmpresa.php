e478d85bcb50c823417a48ebfa85acb994090166
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-13 16:57:32 -0300

    preparação do códio base no arquivo php e css

---
+++ view/CadastrarEmpresa.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="tp-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Gestão, empresa, setor">
+    <meta name="author" content="Leonardo Briguente">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="view/assets/css/cadastrarEmpresa.css">
+
+    <title>SmartGestão</title>
+</head>
+<body>
+<header class="cabecalho">
+        <div class="itens-logo">
+            <img class="logo" src="view/assets/images/logo.png">
+            <h1 class="nome-sistema"><a class="titulo" href="index.php">SmartGestão</a></h1>
+        </div>
+
+        <div class="caminhos">
+            <a class="link" href="view/cadastrarEmpresa.php">Empresas</a>
+            <a class="link link-meio" href="view/cadastrarSetores.php">Setores</a>
+            <a class="link" href="view/relatorios.php">Relatórios</a>
+        </div>
+    </header>
+</body>
+</html>
