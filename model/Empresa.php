93373c3fe456ce8e364b88ea42f2a59f3747f9a6
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-13 23:50:51 -0300

    codificação da class empresa

---
+++ model/Empresa.php
@@ -0,0 +1,52 @@
+<?php
+
+class Empresa
+{
+    private $pdo;
+
+    private $id;
+
+    public function __construct($db)
+    {
+        $this->pdo = $db;
+    }
+
+    public function Cadastrar($razao_social, $nome_fantasia, $cnpj)
+    {
+        $query = "INSERT INTO empresa (razao_social, nome_fantasia, cnpj) VALUES (:razao_social, :nome_fantasia, :cnpj)";
+
+        $stmt = $this->pdo->prepare($query);
+
+        $stmt->bindParam(":razao_social", $razao_social);
+        $stmt->bindParam(":nome_fantasia", $nome_fantasia);
+        $stmt->bindParam(":cnpj", $cnpj);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function ConultarTodos()
+    {
+        $query = "SELECT id, razao_social, nome_fantasia, cnpj FROM empresa";
+
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function ConsultarPorId()
+    {
+        $query = "SELECT id, razao_social, nome_fantasia, cnpj FROM empresa WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(":id", $this->id);
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
+?>
